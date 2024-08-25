591f8145d5bf8458d444b569b1faf0b09a67e8ae
girija-12 <ramagirija95@gmail.com>
2024-08-25 17:00:41 +0530

    Add files via upload

---
+++ SumOfNumber.java
@@ -0,0 +1,13 @@
+package java_lab;
+
+public class SumOfNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=15;
+		int sum=a+b;
+		System.out.println("sum:"+sum);
+		}
+
+}
